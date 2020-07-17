59250ea1f39675047eb498b660adeb60351c1df0
cardgabriel <cardgabriel98@gmail.com>
2020-07-17 11:39:04 -0300

    first commit

---
+++ src/components/Formulario.js
@@ -0,0 +1,70 @@
+import React, { useContext, useState } from "react";
+import { CategoriasContext } from "../context/CategoriasContext";
+import { RecetasContext } from "../context/RecetasContext";
+
+export default function Formulario() {
+  const [busqueda, guardarBusqueda] = useState({
+    nombre: "",
+    categoria: "",
+  });
+
+  const { categorias } = useContext(CategoriasContext);
+
+  const { buscarRecetas, guardarConsultar } = useContext(RecetasContext);
+
+  //Funcion para leer los contenidos
+  const obtenerDatosReceta = (e) => {
+    guardarBusqueda({
+      ...busqueda,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <form
+      className="col-12"
+      onSubmit={(e) => {
+        e.preventDefault();
+        buscarRecetas(busqueda);
+        guardarConsultar(true);
+      }}
+    >
+      <fieldset className="text-center">
+        <legend>Busca por Categoría o Ingrediente</legend>
+      </fieldset>
+
+      <div className="row mt-4">
+        <div className="col-md-4">
+          <input
+            type="text"
+            name="nombre"
+            className="form-control"
+            placeholder="Busca por ingrediente"
+            onChange={obtenerDatosReceta}
+          />
+        </div>
+        <div className="col-md-4">
+          <select
+            className="form-control"
+            name="categoria"
+            onChange={obtenerDatosReceta}
+          >
+            <option value="">--Selecciona Categoría</option>
+            {categorias.map((categoria) => (
+              <option key={categoria.strCategory} value={categoria.strCategory}>
+                {categoria.strCategory}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="col-md-4">
+          <input
+            type="submit"
+            className="btn btn-block btn-primary"
+            value="Buscar Bebidas"
+          ></input>
+        </div>
+      </div>
+    </form>
+  );
+}
